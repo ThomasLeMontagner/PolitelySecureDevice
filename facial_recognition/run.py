8f0d452d5b90ca2398426180225172545c750fe2
ThomasLeMontagner <thomas.le-montagner@hotmail.fr>
2023-03-31 07:18:00 +0200

    Add log file generation

--- facial_recognition/run.py
+++ facial_recognition/run.py
@@ -2,6 +2,8 @@ from voice_generator import *
 from  facial_req import *
 import constants
 import imutils
+import logging
+import datetime
 
 def main():
     # Initialize the engine for face recognition
@@ -13,10 +15,12 @@ def main():
     # List of poeple greeted
     greeted_people = []
 
+    # Set up the logging configuration
+    logging.basicConfig(filename='face_recognition.log', level=logging.INFO, format='%(asctime)s %(message)s')
+
     # loop over frames from the video file stream
     while True:
-        # grab the frame from the threaded video stream and resize it
-        # to 500px (to speedup processing)
+        # grab the frame from the threaded video stream and resize it to 500px (to speedup processing)
         frame = face_recogn.vs.read()
         frame = imutils.resize(frame, width=500)
 
@@ -25,10 +29,16 @@ def main():
 
         face_recogn.plot_face_location(boxes, names, frame)
 
+        # Get the current date and time
+        time = datetime.datetime.now()
+
         for name in names:
             if not name in greeted_people and name != constants.UNKNWON:
                 voice_gen.greet(name)
                 greeted_people.append(name)
+
+                # Log the date, time, and name (or 'unknown')
+                logging.info(f"{time.strftime('%Y-%m-%d %H:%M:%S')} {name}")
 
         key = cv2.waitKey(1) & 0xFF
         # quit when 'q' key is pressed
